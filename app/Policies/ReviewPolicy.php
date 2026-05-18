af5de172930f7854e4cea8a597cf0fef4d2aef61
Sumire Saito <dawn.75night@gmail.com>
2026-05-18 10:04:55 +0900

    feat:レビューポリシーの実装 #12

---
+++ app/Policies/ReviewPolicy.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Review;
+use App\Models\User;
+
+class ReviewPolicy
+{
+    /**
+     * ユーザーがレビューを更新・削除できるか
+     *
+     * @param  User  $user  認証済みユーザー
+     * @param  Review  $review  対象のレビュー
+     */
+    public function update(User $user, Review $review): bool
+    {
+        return $user->id === $review->user_id;
+    }
+}
