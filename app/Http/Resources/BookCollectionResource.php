aa37239d681baac32d04f8bb22acadab310d68f0
Sumire Saito <dawn.75night@gmail.com>
2026-05-19 18:59:57 +0900

    feat:公開APIの書籍一覧取得機能の実装 #26

---
+++ app/Http/Resources/BookCollectionResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BookCollectionResource extends JsonResource
+{
+    /**
+     * 書籍一覧での書籍のリソース表現
+     *
+     * @param  Request  $request  リクエスト
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'author' => $this->author,
+            'genres' => $this->genres->pluck('name'),
+            'image_url' => $this->image_url,
+            'reviews_avg_rating' => $this->reviews_avg_rating ? number_format((float) $this->reviews_avg_rating, 2) : null,
+            'reviews_count' => $this->reviews_count,
+        ];
+    }
+}
