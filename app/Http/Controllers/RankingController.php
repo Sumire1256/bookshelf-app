8f72feb0cad994740265f5f67203c3e59b0a9867
Sumire Saito <dawn.75night@gmail.com>
2026-05-19 16:05:46 +0900

    feat:ランキング表示機能の実装 #25

---
+++ app/Http/Controllers/RankingController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\View\View;
+
+class RankingController extends Controller
+{
+    /**
+     * ランキング画面を表示する
+     */
+    public function index(): View
+    {
+        $rankedBooks = Book::withAvg('reviews', 'rating')->withCount('reviews')->orderByDesc('reviews_avg_rating')->limit(10)->get();
+
+        return view('ranking.index', compact('rankedBooks'));
+    }
+}
